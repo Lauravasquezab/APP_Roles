0fadc1b1a25f280c3704f29bb23b58a62dbce37b
Laura Victoria <80783842+Lauravasquezab@users.noreply.github.com>
2023-10-10 06:26:19 -0500

    Cls_Conexion
    Conexion

---
+++ APP_Roles_Lau/APP_Roles_Lau/cls_conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace APP_Roles_Lau
+{
+    public class cls_conexion
+    {
+        public SqlConnection connection = new SqlConnection("Data Source=DESKTOP-9HKGPQV\\SQLEXPRESS03;Initial Catalog=dbs_roles;Integrated Security=True");
+
+    }
+}
